b0e1fe9d05b232e0a728bafa053f67b78a6cf89c
adamfknapp <adamfknapp@gmail.com>
2021-04-26 23:03:53 -0400

    start quiz game

--- game/static/index.js
+++ game/static/index.js
@@ -2,29 +2,54 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      num1:  Math.ceil(Math.random() * 10),
-      num2:  Math.ceil(Math.random() * 10),
-      response: "",
+      question: "What is Darla?",
+      Answer: "Dog",
+      choice1: "Cat",
+      choice2: "Monkey",
+      choice3: "Horse",
       score: 0
     };
   }
   
   render() { 
     
-    if (this.state.score === 3){
-      return(
-        <div>You Win!</div>
-      )
-    }
-    
     return (
-        <div> 
-          <div>{this.state.num1}+{this.state.num2}</div>
-          <input onKeyPress={this.inputKeyPress} onChange={this.updateResponse} class="bg-gray-300" value= {this.state.response}/>
-          <div> Score: {this.state.score}</div>  
-          <div> You typed: {this.state.response}</div>
-        </div>  
-      );
+      <div class="bg-gradient-to-b from-blue-400 to-blue-900">
+        <div class="flex h-screen justify-center items-center">
+          
+            
+          <h2 class="text-gray-50 text-8xl font-semibold"> {this.state.question} 
+          </h2>
+        
+        <div>
+        <button class="text-xl font-medium text-white-500">{this.state.Answer}</button>
+        <button class="text-xl font-medium text-white-500">{this.state.choice1}</button>
+        <button class="text-xl font-medium text-white-500">{this.state.choice2}</button>
+        <button class="text-xl font-medium text-white-500">{this.state.choice3}</button>
+
+        </div>
+        
+            
+          
+        </div>
+      </div>
+    );
+
+        // <div className="container mx-auto w-1/2"> 
+
+        //   <div>
+        //     <div className="bg-indigo-50 p-10 w-1/2">
+        //       <h1>{this.state.question}</h1>
+        //     </div>
+        //   </div>
+
+        //   <button>{this.state.Answer}</button>
+        //   <button>{this.state.choice1}</button>
+        //   <button>{this.state.choice2}</button>
+        //   <button>{this.state.choice3}</button>
+        //   <div> Score: {this.state.score}</div>  
+        // </div>  
+      //);
     }
 
     inputKeyPress =(event)=>{
