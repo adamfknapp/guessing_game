ec13dc343693db16793b11af272ec84361fb5e79
adamfknapp <adamfknapp@gmail.com>
2021-05-08 08:51:12 -0400

    Update models.py

--- game/models.py
+++ game/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Game(models.Model):
     game_id = models.AutoField(primary_key=True)
     score = models.IntegerField()
-    num_quest = models.IntegerField()
+    max_questions = models.IntegerField()
 
     def __str__(self):
-        return f"game_id: {self.game_id} | score: {self.score} | num_quest: {self.num_quest}"
+        return f"game_id: {self.game_id} | score: {self.score} | max_questionst: {self.max_questions}"
